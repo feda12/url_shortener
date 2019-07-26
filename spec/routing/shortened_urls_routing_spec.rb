804e3b10dba90caad6a12114ec2c192af39fa7f6
feda12 <lecam@stolaf.edu>
2019-07-26 10:52:34 -0500

    Fixes routing specs after the change to resource path

--- spec/routing/shortened_urls_routing_spec.rb
+++ spec/routing/shortened_urls_routing_spec.rb
@@ -3,28 +3,16 @@ require "rails_helper"
 RSpec.describe ShortenedUrlsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/shortened_urls").to route_to("shortened_urls#index")
+      expect(:get => "/").to route_to("shortened_urls#index")
     end
 
     it "routes to #show" do
-      expect(:get => "/shortened_urls/1").to route_to("shortened_urls#show", :id => "1")
+      expect(:get => "/slug").to route_to("shortened_urls#show", :id => "slug")
     end
 
 
     it "routes to #create" do
-      expect(:post => "/shortened_urls").to route_to("shortened_urls#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/shortened_urls/1").to route_to("shortened_urls#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/shortened_urls/1").to route_to("shortened_urls#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/shortened_urls/1").to route_to("shortened_urls#destroy", :id => "1")
+      expect(:post => "/").to route_to("shortened_urls#create")
     end
   end
 end
